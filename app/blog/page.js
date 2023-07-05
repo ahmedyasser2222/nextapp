3d0dabb524cea527be64dd108499d943eaf520c6
Ahmed Yasser <ah01211293047@gmail.com>
2023-07-05 19:24:20 +0300

    first commit

--- app/blog/page.js
+++ app/blog/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts",
+  const res = await fetch(`${process.env.DOMAN}/api/posts`,
   { next: { revalidate: 10 } }
   );
 
